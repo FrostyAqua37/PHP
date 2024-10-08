ab878d48e61ab15178f86e2818fee483f638f269
FrostyAqua37 <eivindc@uia.no>
2024-10-08 23:26:59 +0200

    Modul 4 done

--- Modul 4/oppgave_4.php
+++ Modul 4/oppgave_4.php
@@ -4,25 +4,36 @@
 <?php
 $roomtypes = [
     $singleroom = [
-        $name = "Single Room",
-        $space = 1,
-        $price = "1500 kr",
-        $description = "A room for one person."
+        "name" => "Single Room",
+        "space" => 1,
+        "price" => "1500 kr",
+        "description" => "A room for one person."
     ],
 
     $doubleroom = [
-        $name = "Double Room",
-        $space = 2,
-        $price = "2200 kr",
-        $description = "A room for two people."
+        "name" => "Double Room",
+        "space" => 2,
+        "price" => "2200 kr",
+        "description" => "A room for two people."
     ],
 
     $junior_suite = [
-        $name = "Junior Suite",
-        $space = 2,
-        $price = "2500 kr",
-        $description = "A room for two people with a living room."
+        "name" => "Junior Suite",
+        "space" => 2,
+        "price" => "2500 kr",
+        "description" => "A room for two people with a living room."
     ]
 ];
-
 ?>
+
+
+<table> 
+    <?php 
+    foreach($roomtypes as $types) {
+        echo "<tr><td>" . "<strong>Roomtype: </strong>" . $types["name"] . "</td></tr>";
+        echo "<tr><td>" . "<strong>Amount: </strong>" . $types["space"] . "</td></tr>";
+        echo "<tr><td>" . "<strong>Price: </strong>" . $types["price"] . "</td></tr>";
+        echo "<tr><td>" . "<strong>Description: </strong>" . $types["description"] . "</td></tr>";
+    }
+
+    ?>
