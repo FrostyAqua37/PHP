d03b46dea3de8965ab8052b9d4e25ec1c40b14a1
FrostyAqua37 <eivindc@uia.no>
2024-10-21 19:51:07 +0200

    Fullførte oppgave 1 og 2.

--- Modul 5/Oppgave_4.php
+++ Modul 5/Oppgave_4.php
@@ -2,11 +2,13 @@
 
 $array = [5, 3, 0, 3, 0, 5, 7, 7, 9, 10, 20]; // Added some more values to check if the code works as expected.
 
-$frequency = array_count_values($array);
+echo '<pre>'; print_r($array); echo '</pre>';
 
-//Finds the values that only appears once and the key, and prints them both out.
-foreach ($frequency as $value => $occurrences) {
-    if($occurrences === 1) {
+$frequency = array_count_values($array); //Finds the occurrences of each value
+
+//Finds the values that only appears once
+foreach ($frequency as $value => $occurrence) {
+    if($occurrence === 1) {
         //Finds the key
         $key = array_search($value, $array);
         echo "Element with the key of [" . $key . "] and value of (" . $value . ") appears only once in the array. <br>";
--- Modul 5/oppgave_5.php
+++ Modul 5/oppgave_5.php
@@ -1,15 +1,17 @@
 <?php
     function anagram ($word1, $word2) {
-        $word1 = strtolower($word1);
-        $word2 = strtolower($word2);
-        //Finds the frequency of letters in the variables and matches it against eachother to check for anagrams
+        $word1 = strtolower(trim($word1));
+        $word2 = strtolower(trim($word2));
+        //Finds and matches the amount of each letter in both variables 
         if (count_chars($word1, 1) == count_chars($word2, 1)){
-            echo $word1 . " is an anagram of " . $word2;
+            echo $word1 . " is an anagram of " . $word2 . ".";
         } else {
-            echo $word1 . " is not an anagram of " . $word2;
+            echo $word1 . " is not an anagram of " . $word2 . ".";
         }
     }
 
     echo(anagram("night", "Thing") . "<br>");
+    echo(anagram("Sun", "Moon") . "<br>");
+
     
 ?>
