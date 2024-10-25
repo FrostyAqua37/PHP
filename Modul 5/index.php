c77ba392babce1c6d3254f4e83e6f64c2437bec8
FrostyAqua37 <eivindc@uia.no>
2024-10-25 14:12:30 +0200

    index.php til modul 5

--- Modul 5/index.php
+++ Modul 5/index.php
@@ -2,10 +2,9 @@
 <strong>Modul 5</strong><br>
 
 <?php
-
 echo "<a href=oppgave_1.pdf download>Lenke til oppgave 1.</a><br>";
 echo "Filene til oppgave 2 ligger i mappen. <br>";
-echo "<a href=oppgave_1.pdf download>Lenke til oppgave 3.</a><br>";
+echo "<a href=oppgave_3.mkv download>Lenke til oppgave 3.</a><br>";
 echo "<a href=oppgave_4.php target=_blank>Lenke til oppgave 4.</a><br>";
 echo "<a href=oppgave_5.php target=_blank>Lenke til oppgave 5.</a><br>";
 ?>
